4e9a7acf2173f83d864fd6fbbb4c71dace316c59
Jafar Jafarov <jafestro@gmail.com>
2023-02-09 13:00:21 +0400

    Flight and Booking Alpha version 0.0.2 ADDED Time stamps

--- src/main/java/az/iktlab/flights_and_bookings/entity/FlightEntity.java
+++ src/main/java/az/iktlab/flights_and_bookings/entity/FlightEntity.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
 import java.time.LocalDate;
@@ -19,12 +21,22 @@ import java.time.LocalTime;
 public class FlightEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "flight_id")
     private Long flightId;
+    @Column(name = "date")
     private LocalDate date;
+    @Column(name = "time")
     private LocalTime time;
+    @Column(name = "departure")
     private String departure;
+    @Column(name = "arrival")
     private String arrival;
+    @Column(name = "available_seats")
     private int availableSeats;
+    @Column(name = "created_at")
+    @CreationTimestamp
     private LocalDateTime createdAt;
+    @Column(name = "updated_at")
+    @UpdateTimestamp
     private LocalDateTime updatedAt;
 }
--- src/main/java/az/iktlab/flights_and_bookings/entity/PassengerEntity.java
+++ src/main/java/az/iktlab/flights_and_bookings/entity/PassengerEntity.java
@@ -4,6 +4,8 @@ import az.iktlab.flights_and_bookings.model.Booking;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -26,8 +28,10 @@ public class PassengerEntity {
     @Column(unique = true,nullable = false)
     private String passportNumber;
     @Column(name = "created_at")
+    @CreationTimestamp
     private LocalDateTime createdAt;
     @Column(name = "updated_at")
+    @UpdateTimestamp
     private LocalDateTime updatedAt;
 
 }
